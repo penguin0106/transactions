e43e94f309de1c227336fb222bc3408f7d7f77ca
penguin0106 <yabmaxim85@gmail.com>
2024-05-24 18:49:50 +0300

    new

--- wallet/models/transaction.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Transaction struct {
-	ID        int     `json:"id"`
-	UserID    int     `json:"user_id"`
-	Amount    float64 `json:"amount"`
-	Type      string  `json:"type"`
-	Timestamp string  `json:"timestamp"`
-}
